1adab76eaf4edcf45227610cef326efbe11a09ad
isaac <isaac.diez@proton.me>
2024-11-13 16:23:46 +0100

    Commit 1: Reformat application properties, created conference and speaker classes.

--- src/main/java/io/bcn/springConference/model/Conference.java
+++ src/main/java/io/bcn/springConference/model/Conference.java
@@ -1,4 +1,56 @@
 package io.bcn.springConference.model;
 
-public class Conference {
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+    @Entity
+    @Table(name = "conferences")
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    public class Conference {
+
+        @Id
+        @GeneratedValue(generator = "UUID")
+        @GenericGenerator(name = "UUID",
+                strategy = "org.hibernate.id.UUIDGenerator")
+        @Column(name = "id", updatable = false,
+                nullable = false)
+        private UUID id;
+
+        @Column(nullable = false)
+        private String date;
+
+        @Column(name = "youtube_link")
+        private String linkToYouTubeVideo;
+
+        @Column(nullable = false)
+        private String title;
+
+        @Column(name = "conference_name",
+                nullable = false)
+        private String conferenceName;
+
+        @Column(columnDefinition = "TEXT")
+        private String content;
+
+        private Integer duration;
+
+        private String room;
+
+        @ManyToOne
+        @JoinColumn(name = "book_id",
+                nullable = false)
+        private Book book;
+
+        @ManyToOne
+        @JoinColumn(name = "speaker_id",
+                nullable = false)
+        private Speaker speaker;
+
 }
--- src/main/java/io/bcn/springConference/model/Speaker.java
+++ src/main/java/io/bcn/springConference/model/Speaker.java
@@ -1,4 +1,37 @@
 package io.bcn.springConference.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "speakers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Speaker {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "id", updatable = false,
+            nullable = false)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String bio;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "speaker")
+    private List<Conference> conferences;
 }
